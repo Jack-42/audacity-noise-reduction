27987f7fae2bdcccbbece9e7265b9378aad41bc6
Jack-42 <jakob-robert@hotmail.de>
2020-10-10 15:44:56 +0200

    Remove TODO

--- noisereduction/BufferedTrackUtils.h
+++ noisereduction/BufferedTrackUtils.h
@@ -6,7 +6,6 @@
 
 namespace BufferedTrackUtils
 {
-    // TODO: add t0 and t1 to only read part of track
     std::vector<BufferedInputTrack> readTracksFromContext(const SndContext& ctx, size_t t0 = 0, size_t t1 = 0);
     BufferedInputTrack readOneTrackFromContext(const SndContext &ctx, int channel, size_t t0 = 0, size_t t1 = 0);
     void writeTracksToFile(const std::vector<BufferedOutputTrack> &tracks, const char* path, int channels, int sampleRate);
