5ad2e54bcb2d1277aea795a509242c9c05c2cd58
Jack-42 <jakob-robert@hotmail.de>
2020-11-22 20:58:50 +0100

    Copy libsndfile dll in batch script

--- build_driver.bat
+++ build_driver.bat
@@ -3,4 +3,5 @@ cd build
 cmake ..
 cmake --build . --target noisereduction_driver
 cd ..
+copy /y "C:\Program Files\Mega-Nerd\libsndfile\bin\libsndfile-1.dll" ".\build\Debug\libsndfile-1.dll"
 PAUSE
